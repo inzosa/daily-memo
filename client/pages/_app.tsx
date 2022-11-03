481768c9104b9c79dcdbcdf8e9bb075c1a1ad949
Jaehak Kim <jaehakkim@Jaehakui-MacBookPro.local>
2022-11-03 11:05:42 +0900

    redux

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import { NextPage } from 'next';
+import { wrapper } from '../store/store';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default wrapper.withRedux(MyApp);
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,27 +1,35 @@
 import Head from 'next/head'
 import { Icon } from '../components/atoms/Icon'
+import { wrapper } from '../store/store'
 import styles from '../styles/Home.module.css'
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { loadData, startClock, tickClock } from '../store/actions/actExample';
+import { useStore } from 'react-redux';
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const store:any = useStore();
+  const clock = store.getState().rdcExample.lastUpdate;
+
+
+  useEffect(() => {
+    dispatch(startClock());
+    console.log('clock:',clock);
+  }, []);
   return (
     <div className={styles.container}>
       <main>
-        hot load3
+        <p>time: {clock}</p>
         <Icon src="/bulldog.png" width='10vw' height='10vw' />
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+       
           developed by Moon Ju-wan
-          {/* <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span> */}
-        </a>
+       
       </footer>
     </div>
   )
 }
+
